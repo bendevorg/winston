0f0de799f619dcd47e18438d64a5b3ca674e7aa1
Guilherme Conti <guibasconti@gmail.com>
2017-10-27 00:39:35 -0200

    Dont close DB when tests are done

--- test/runTests.js
+++ test/runTests.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 let testInfo = require('./testInfo');
 
 //  Start tests
-require('./auth/root')(testInfo)
+require('./auth/root')(testInfo);
 
 //  Close connections
-require('./utils/closeConnection')();
+//require('./utils/closeConnection')();
