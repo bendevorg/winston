16d3d6c508e7d2537df64d9d2ac42b3f15ef0216
leonardo a <leonardo.akato@gmail.com>
2018-01-05 13:44:35 -0200

    getting Users BTAG by sending TelegramID

--- server/controllers/deleteUser.js
+++ server/controllers/deleteUser.js
@@ -6,7 +6,7 @@
 const logger = require('../../tools/logger');
 const database = require('../models/database');
 const validator = require('../utils/validator');
-// const constants = require('../utils/constants');
+const constants = require('../utils/constants');
 
 /**
  * Create a new user API
@@ -36,46 +36,45 @@ const validator = require('../utils/validator');
 // };
 
 module.exports = (req, res) => {
-//   let { id, battleTag } = req.body;
+  let { id } = req.body;
+  id = parseInt(id, 10);
+  console.log("id: " + id);
   
-//   if (!validator.isValidInteger(id))
-//     return res.status(400).json({
-//       msg: constants.messages.error.INVALID_ID
-//     });
+  if (!validator.isValidInteger(id))
+    return res.status(400).json({
+      msg: constants.messages.error.INVALID_ID
+    });
 
-//   if (!validator.isValidBattleTag(battleTag))
-//     return res.status(400).json({
-//       msg: constants.messages.error.INVALID_BATTLETAG
-//     });
+  database.user.findById(id).then((user) => {
+    return res.status(201).json({
+      msg: user.battleTag
+    });
+  });
+  // database.user
+  //   .findById(id)
+  //   .then(existingUser => {
+  //     if (existingUser)
+  //       return res.status(400).json({
+  //         msg: constants.messages.error.EXISTING_USER
+  //       });
 
-//   id = parseInt(id, 10);
-//   battleTag = battleTag.trim();
-  
-//   database.user
-//     .findById(id)
-//     .then(existingUser => {
-//       if (existingUser)
-//         return res.status(400).json({
-//           msg: constants.messages.error.EXISTING_USER
-//         });
-
-//       let newUser = database.user.build({ id, battleTag });
-//       newUser
-//         .save()
-//         .then(() => {
-//           return res.status(201).json({
-//             msg: constants.messages.success.USER_REGISTERED
-//           });
-//         })
-//         .catch(err => {
-//           logger.error(err);
-//           return res
-//             .status(500)
-//             .json({ msg: constants.messages.error.UNEXPECTED });
-//         });
-//     })
-//     .catch(err => {
-//       logger.error(err);
-//       return res.status(500).json({ msg: constants.messages.error.UNEXPECTED });
-//     });
+  //     let newUser = database.user.build({ id, battleTag });
+  //     newUser
+  //       .save()
+  //       .then(() => {
+  //         return res.status(201).json({
+  //           msg: constants.messages.success.USER_REGISTERED
+  //         });
+  //       })
+  //       .catch(err => {
+  //         logger.error(err);
+  //         return res
+  //           .status(500)
+  //           .json({ msg: constants.messages.error.UNEXPECTED });
+  //       });
+  //   })
+  //   .catch(err => {
+  //     logger.error(err);
+  //     return res.status(500).json({ msg: constants.messages.error.UNEXPECTED });
+  //   });
 };
