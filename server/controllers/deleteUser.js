1d5c5810adbc8e7012daf7f24a1e9525b766efdc
leonardo a <leonardo.akato@gmail.com>
2018-01-05 14:47:36 -0200

    boa tarde meu nome eh sr funfando e deletando

--- server/controllers/deleteUser.js
+++ server/controllers/deleteUser.js
@@ -45,11 +45,37 @@ module.exports = (req, res) => {
       msg: constants.messages.error.INVALID_ID
     });
 
-  database.user.findById(id).then((user) => {
-    return res.status(201).json({
-      msg: user.battleTag
+  // database.user.findById(id).then((user) => {
+  //   return res.status(201).json({
+  //     msg: user.battleTag
+  //   });
+  // });
+  
+
+  database.user.destroy({
+    where: {
+      id: id //this will be your id that you want to delete
+    }
+  }).then(function(rowDeleted){ // rowDeleted will return number of rows deleted
+    if(rowDeleted === 1){
+      console.log('Deleted successfully');
+      return res.status(201).json({
+        msg: 'Your id "' + id + '" was successfully found and exterminated!'
+      });
+    } else {
+      return res.status(201).json({
+        msg: 'Telegram ID not found.'
+      });
+    }
+  }, function(err){
+    console.log(err);
+    return res.status(400).json({
+      msg: 'failed for some reason.'
     });
   });
+
+  
+
   // database.user
   //   .findById(id)
   //   .then(existingUser => {
