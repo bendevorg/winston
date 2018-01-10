927346493e4c03f1abe5ebd97319e0ce53b67406
leonardo a <leonardo.akato@gmail.com>
2018-01-10 01:56:46 -0200

    added del not-found user CI

--- test/register/deleteUser.js
+++ test/register/deleteUser.js
@@ -29,23 +29,23 @@ module.exports = describe('Register user use cases', () => {
       });
   });
 
-  // it('Register user with invalid battleTag', done => {
-  //   api
-  //     .post(constants.urls.PREFIX + constants.urls.REGISTER_USER)
-  //     .set(constants.users.validApiKeyOne.header)
-  //     .send(constants.register.invalidBattleTag)
-  //     .end((err, res) => {
-  //       if (err) {
-  //         done(err);
-  //       } else {
-  //         expect(res.body)
-  //           .to.have.property('msg')
-  //           .to.equal(appConstants.messages.error.INVALID_BATTLETAG);
-  //         expect(res.status, 'Status').to.equal(400);
-  //         done();
-  //       }
-  //     });
-  // });
+  it('Delete valid inexistent user', done => {
+    api
+      .post(constants.urls.PREFIX + constants.urls.DELETE_USER)
+      .set(constants.users.validApiKeyOne.header)
+      .send(constants.register.inexistentId)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        } else {
+          expect(res.body)
+            .to.have.property('msg')
+            .to.equal(appConstants.messages.error.USER_NOT_FOUND);
+          expect(res.status, 'Status').to.equal(400);
+          done();
+        }
+      });
+  });
 
   it('Delete user with valid input', done => {
     api
